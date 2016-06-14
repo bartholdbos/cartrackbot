d03c9a39e28f84af4262b10a62f0db280e2f09aa
Barthold Bos <bartholdbos@hotmail.com>
2016-06-14 23:23:49 +0200

    Add MySQL and location updating

--- main.go
+++ main.go
@@ -1,17 +1,23 @@
 package main
 
-import(
-	"github.com/bartholdbos/golegram"
-	"log"
+import (
+	"database/sql"
 	"fmt"
+	"github.com/bartholdbos/golegram"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
 	"net/http"
+	"strconv"
 )
 
-var bot golegram.Bot
+var db *sql.DB
+var bot *golegram.Bot
 var config Configuration
 
 func handleErr(err error) {
-	log.Fatal(err)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func handlePing(out http.ResponseWriter, in *http.Request) {
@@ -19,29 +25,48 @@ func handlePing(out http.ResponseWriter, in *http.Request) {
 }
 
 func handleUpdate(update golegram.Update) {
-	fmt.Println(update.Message.Text)
+	if update.Message.Location.Longitude == 0 && update.Message.Location.Latitude == 0 {
+		bot.SendMessage(strconv.Itoa(int(update.Message.Chat.Id)), "test", false, "")
+	} else {
+		row, err := db.Query("SELECT `car` FROM `users` WHERE `id` = ?;", update.Message.Chat.Id)
+		handleErr(err)
+		row.Next()
+
+		var car int
+		err1 := row.Scan(&car)
+		handleErr(err1)
+
+		_, err2 := db.Query("UPDATE `cars` SET `longitude`=?,`latitude`=? WHERE `id` = ?;", update.Message.Location.Longitude, update.Message.Location.Latitude, car)
+		handleErr(err2)
+
+		bot.SendMessage(strconv.Itoa(int(update.Message.Chat.Id)), "Locatie bijgewerkt", false, "")
+	}
 }
 
 func setupConfig() {
 	var err error
 	config, err = load_config()
 
-	if err != nil {
-		handleErr(err)
-	}
+	handleErr(err)
 
 	fmt.Println("Config loaded")
 }
 
 func setupBot() {
 	var err error
-	bot, err := golegram.NewBot(config.TelegramBotToken)
+	bot, err = golegram.NewBot(config.TelegramBotToken)
 
-	if err != nil {
-		handleErr(err)
-	}
+	handleErr(err)
 
 	fmt.Println("Bot created: " + bot.User.Username)
+}
+
+func setupMySQL() {
+	var err error
+	db, err = sql.Open("mysql", config.generateDSN())
+
+	handleErr(err)
+	handleErr(db.Ping())
 }
 
 func setupWebhook() {
@@ -50,13 +75,12 @@ func setupWebhook() {
 	fmt.Println("Starting Webhook")
 	err := golegram.StartWebhook(config.Webhook.Port, config.Webhook.Public, config.Webhook.Private)
 
-	if err != nil {
-		handleErr(err)
-	}
+	handleErr(err)
 }
 
 func main() {
 	setupConfig()
 	setupBot()
+	setupMySQL()
 	setupWebhook()
 }
